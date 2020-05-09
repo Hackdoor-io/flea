79428cf6cbddb124d291e4023ca00637fb20365a
Fausto Quaggia <fausto.quaggia@openmindonline.it>
2020-05-09 17:33:54 +0200

    refactor(anchors): code review

--- src/anchors/index.ts
+++ src/anchors/index.ts
@@ -4,44 +4,38 @@
  * @returns {RegExp}
  */
 const headingRegex = (): RegExp =>
-  new RegExp('(?<=(?!h1|h2|h3|h4|h5|h6)>)(.+?)(?=</+?(?=h1|h2|h3|h4|h5|h6))', 'ig')
+  new RegExp('(?<=(?!h(|1|2|3|4|5|6))>)(.+?)(?=</+?(?=h(|1|2|3|4|5|6)))', 'ig')
 
 /**
  * @function slugify
  * @param {String} text
  * @returns {String}
  */
-const slugify = (text: string): string => {
-  // trim string
-  text = text.replace(/^\s+|\s+$/g, '')
-
-  // lowercase
-  text = text.toLowerCase()
-
-  // remove accents, swap à for a, ñ for n, etc...
-  const from = 'àáäâèéëêìíïîòóöôùúüûñç·/_,:;'
-  const to = 'aaaaeeeeiiiioooouuuunc------'
-  for (let i = 0, l = from.length; i < l; i++) {
-    text = text.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i))
-  }
-
-  text = text
-    .replace(/<[^>]*>/gi, '') // remove internal html tags
-    .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-    .replace(/\s+/g, '-') // collapse whitespace and replace by -
-    .replace(/-+/g, '-') // collapse dashes
-
-  return text
-}
+const slugify = (text: string): string =>
+  text
+    .replace(/^\s+|\s+$/g, '')
+    .toLowerCase()
+    .replace(/<[^>]*>/gi, '')
+    .replace(/[àáâãäå]/gi, 'a')
+    .replace(/[èéëê]/gi, 'e')
+    .replace(/[ìíïî]/gi, 'i')
+    .replace(/[òóöô]/gi, 'o')
+    .replace(/[ùúüû]/gi, 'u')
+    .replace(/[ñ]/gi, 'n')
+    .replace(/[ç]/gi, 'c')
+    .replace(/[^a-z0-9 -]/gi, '')
+    .replace(/\s+/gi, '-')
+    .split('')
+    .map(char => char.replace(/[^\w]/i, '-'))
+    .join('')
 
 /**
  * @function createAnchor
  * @param {String} text
  * @returns {String}
  */
-const createAnchor = (text: string): string => {
-  return '<a href="#' + slugify(text) + '" class="h-anchor">#</a>'
-}
+const createAnchor = (text: string): string =>
+  '<a href="#' + slugify(text) + '" class="h-anchor">#</a>'
 
 /**
  * @function anchors
