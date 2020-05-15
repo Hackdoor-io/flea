bfd922f92a154fdabe234c22a9882aaa6f5cb45d
Fausto Quaggia <fausto.quaggia@openmindonline.it>
2020-05-15 10:13:27 +0200

    test(anchors): add test for html with no headings inside

--- src/anchors/index.test.ts
+++ src/anchors/index.test.ts
@@ -33,3 +33,18 @@ test('Testing anchors with nested html tags', () => {
     <h2><a id="lorem-ipsum-dolor-sit-amet" href="#lorem-ipsum-dolor-sit-amet" class="h-anchor">#</a><i>Lorem Ipsum Dolor Sit Amet</i></h2>
   `)
 })
+
+test('Testing html with no headings', () => {
+  const replaceAnchors = anchors()
+  const HTMLInput = `
+    <strong>Testing html with no headings</strong>
+    <p>Lorem Ipsum Dolor Sit Amet lorem ipsum</p>
+    <p><i>Lorem Ipsum Dolor Sit Amet</i></p>
+  `
+
+  expect(replaceAnchors(HTMLInput)).toBe(`
+    <strong>Testing html with no headings</strong>
+    <p>Lorem Ipsum Dolor Sit Amet lorem ipsum</p>
+    <p><i>Lorem Ipsum Dolor Sit Amet</i></p>
+  `)
+})
